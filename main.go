d1a89b2a55f2a0f219d53f67b195e28ddcd05a21
amedab <alexander.bjornsrud@amedia.no>
2022-04-19 05:09:15 -0700

    .

--- main.go
+++ main.go
@@ -9,14 +9,15 @@ import (
 var out string
 
 func init() {
-	out := os.Getenv("SECRET")
+	out = os.Getenv("SECRET")
+}
+
+func print(w ResponseWriter, req *Request) {
+	w.Write("<pre>%s</pre>".format(out))
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", func(w ResponseWriter, req *Request) {
-		w.Write("<pre>%s</pre>".format(out))
-	})
-
+	r.HandleFunc("/", print)
 	log.Errorf("Server crashed %s", http.ListenAndServe(":31337", r).String())
 }
